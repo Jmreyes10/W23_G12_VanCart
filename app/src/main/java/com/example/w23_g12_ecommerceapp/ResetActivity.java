96f3bc199f78d99dd393d77b857e36393dfdeab8
Shaun Ritty <shaunritty@gmail.com>
2023-03-16 15:09:08 -0700

    Notification extension added

--- app/src/main/java/com/example/w23_g12_ecommerceapp/ResetActivity.java
+++ app/src/main/java/com/example/w23_g12_ecommerceapp/ResetActivity.java
@@ -1,8 +1,15 @@
 package com.example.w23_g12_ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +24,7 @@ public class ResetActivity extends AppCompatActivity {
     EditText txtComfirm_password;
     Button btnChange_Password;
     DBHelper DB;
+    String usernamehere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +36,23 @@ public class ResetActivity extends AppCompatActivity {
         txtComfirm_password=findViewById(R.id.txtComfirm_password);
         btnChange_Password=findViewById(R.id.btnChange_Password);
         Intent intent=getIntent();
-        txtViewReset.setText(intent.getStringExtra("username"));
+
+        usernamehere=intent.getStringExtra("username");
+        txtViewReset.setText("Enter the new password for "+usernamehere);
         DB=new DBHelper(this);
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)    {
+            NotificationChannel channel=new NotificationChannel("My Notification","My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
 
         btnChange_Password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String user = txtViewReset.getText().toString();
+                String user = usernamehere;
                 String password = txtpassword.getText().toString();
                 String compassword = txtComfirm_password.getText().toString();
 
@@ -44,12 +61,21 @@ public class ResetActivity extends AppCompatActivity {
 
                     Boolean checkpasswordupdate = DB.updatepassword(user, password);
                     if (checkpasswordupdate == true) {
+                        NotificationCompat.Builder builder=new NotificationCompat.Builder(ResetActivity.this,"My Notification");
+                        builder.setContentTitle("New Notification");
+                        builder.setContentText("Password changed successfully for " +user);
+                        builder.setSmallIcon(R.mipmap.ic_launcher_logo);
+                        builder.setAutoCancel(true);
+                        NotificationManagerCompat managerCompat=NotificationManagerCompat.from(ResetActivity.this);
+                        managerCompat.notify(1,builder.build());
                         Intent intent = new Intent(getApplicationContext(), LoginNewActivity.class);
                         startActivity(intent);
                         Toast.makeText(ResetActivity.this, "Password updated successfully", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(ResetActivity.this, "Password not updated successfully", Toast.LENGTH_SHORT).show();
                     }
+
+                }else{
                     Toast.makeText(ResetActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
                 }
             }
