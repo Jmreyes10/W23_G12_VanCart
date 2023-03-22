127644a493cf289a65affde04ca5664e74cd9693
Harkirat0 <amh2harkirat@gmail.com>
2023-03-22 09:13:52 -0700

    Customized and created the nav slider

--- app/src/main/java/com/example/w23_g12_ecommerceapp/DrawerListAdapter.java
+++ app/src/main/java/com/example/w23_g12_ecommerceapp/DrawerListAdapter.java
@@ -1,38 +1,36 @@
-package com.example.w23_g12_ecommerceapp;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.List;
-
-public class DrawerListAdapter extends ArrayAdapter {
-    public DrawerListAdapter(Context context, List objects) {
-        super(context, 0, objects);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        if(convertView == null){
-            LayoutInflater inflater = (LayoutInflater)parent.getContext().
-                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.drawer_list_item, null);
-        }
-
-
-        ImageView icon = (ImageView) convertView.findViewById(R.id.icon);
-        TextView feature = (TextView) convertView.findViewById(R.id.feature);
-
-        DrawerItem item = (DrawerItem) getItem(position);
-        icon.setImageResource(item.getIconId());
-        feature.setText(item.getFeature());
-
-        return convertView;
-    }
-
-}
+//package com.example.w23_g12_ecommerceapp;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import java.util.List;
+//
+//public class DrawerListAdapter extends ArrayAdapter {
+//    public DrawerListAdapter(Context context, List objects) {
+//        super(context, 0, objects);
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//
+//        if(convertView == null){
+//            LayoutInflater inflater = (LayoutInflater)parent.getContext().
+//                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            convertView = inflater.inflate(R.layout.drawer_list_item, null);
+//        }
+//
+//
+//        TextView feature = (TextView) convertView.findViewById(R.id.feature);
+//
+//        DrawerItem item = (DrawerItem) getItem(position);
+//        feature.setText(item.getFeature());
+//
+//        return convertView;
+//    }
+//
+//}
--- app/src/main/java/com/example/w23_g12_ecommerceapp/MenuActivity.java
+++ app/src/main/java/com/example/w23_g12_ecommerceapp/MenuActivity.java
@@ -1,34 +1,51 @@
 package com.example.w23_g12_ecommerceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
-    private ListView drawerList;
-    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
-    private CharSequence activityTitle;
-    private CharSequence itemTitle;
-    private String[] tagTitles;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle;
+
+    private FirebaseUser user;
+    private FirebaseAuth auth;
+//    private ListView drawerList;
+//    private ActionBarDrawerToggle drawerToggle;
+//
+//    private CharSequence activityTitle;
+//    private CharSequence itemTitle;
+//    private String[] tagTitles;
 //    TextView txtViewUsername;
 //    DBHelper DB;
 
@@ -38,10 +55,32 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        itemTitle = activityTitle = getTitle();
-        tagTitles = getResources().getStringArray(R.array.Tags);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        getSupportActionBar().setTitle("VanCart");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_bg));
+
+//        itemTitle = activityTitle = getTitle();
+//        tagTitles = getResources().getStringArray(R.array.Tags);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        //drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        navigationView.bringToFront();
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_feature01);
+
+
+//        drawerList = (ListView) findViewById(R.id.left_drawer);
 
         ImageSlider imageSlider = findViewById(R.id.imageSlider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
@@ -61,90 +100,138 @@ public class MenuActivity extends AppCompatActivity {
 
 
         //Create elements of the list
-        ArrayList<DrawerItem> items = new ArrayList<DrawerItem>();
-        items.add(new DrawerItem(tagTitles[0], R.drawable.icon_feature01));
-        items.add(new DrawerItem(tagTitles[1], R.drawable.icon_feature02));
-        items.add(new DrawerItem(tagTitles[2], R.drawable.icon_feature03));
-        items.add(new DrawerItem(tagTitles[3], R.drawable.icon_feature04));
-        items.add(new DrawerItem(tagTitles[4], R.drawable.icon_feature05));
-        items.add(new DrawerItem(tagTitles[5], R.drawable.icon_feature06));
-
-        // Relate the adapter and the DrawerItemClickListener
-        drawerList.setAdapter(new DrawerListAdapter(this, items));
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+//        ArrayList<DrawerItem> items = new ArrayList<DrawerItem>();
+//        items.add(new DrawerItem(tagTitles[0], R.drawable.icon_feature01));
+//        items.add(new DrawerItem(tagTitles[1], R.drawable.icon_feature02));
+//        items.add(new DrawerItem(tagTitles[2], R.drawable.icon_feature03));
+//        items.add(new DrawerItem(tagTitles[3], R.drawable.icon_feature04));
+//        items.add(new DrawerItem(tagTitles[4], R.drawable.icon_feature05));
+//        items.add(new DrawerItem(tagTitles[5], R.drawable.icon_feature06));
+//
+//        // Relate the adapter and the DrawerItemClickListener
+//        drawerList.setAdapter(new DrawerListAdapter(this, items));
+//        drawerList.setOnItemClickListener(new DrawerItemClickListener());
 
         // Crete ActionBarDrawerToggle for opening and closing
-        drawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                R.string.drawer_open,
-                R.string.drawer_close
-        ) {
-            public void onDrawerClosed(View view) {
-                getSupportActionBar().setTitle(itemTitle);
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                getSupportActionBar().setTitle(activityTitle);
-
-            }
-        };
-
-        drawerLayout.setDrawerListener(drawerToggle);
-
+//        drawerToggle = new ActionBarDrawerToggle(
+//                this,
+//                drawerLayout,
+//                R.string.drawer_open,
+//                R.string.drawer_close
+//        ) {
+//            public void onDrawerClosed(View view) {
+//                getSupportActionBar().setTitle(itemTitle);
+//            }
+//
+//            public void onDrawerOpened(View drawerView) {
+//                getSupportActionBar().setTitle(activityTitle);
+//
+//            }
+//        };
+//
+//        drawerLayout.setDrawerListener(drawerToggle);
+//
+//    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        if (drawerToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            Toast.makeText(MenuActivity.this, "SelectItem called", Toast.LENGTH_SHORT).show();
+//            selectItem(position);
+//        }
+//    }
+//
+//    private void selectItem(int position) {
+//
+//        // Start the selected feature activity after closing the drawer
+//        drawerList.setItemChecked(position, true);
+//        drawerLayout.closeDrawer(drawerList);
+//
+//        switch (position){
+//            case 0:
+////                startActivity(new Intent(this, Feature01Activity.class));
+//                break;
+//            case 1:
+//                startActivity(new Intent(this, Feature02Activity.class));
+//                break;
+//            case 2:
+//                startActivity(new Intent(this, Feature03Activity.class));
+//                break;
+//            case 3:
+//                startActivity(new Intent(this, Feature04Activity.class));
+//                break;
+//            case 4:
+//                startActivity(new Intent(this, Feature05Activity.class));
+//                break;
+//            case 5:
+//                startActivity(new Intent(this, Feature06Activity.class));
+//                break;
+//        }
+//
+//    }
+//
+//    @Override
+//    protected void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//        drawerToggle.syncState();
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (drawerToggle.onOptionsItemSelected(item)) {
+        if (toggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Toast.makeText(MenuActivity.this, "SelectItem called", Toast.LENGTH_SHORT).show();
-            selectItem(position);
-        }
-    }
 
-    private void selectItem(int position) {
-
-        // Start the selected feature activity after closing the drawer
-        drawerList.setItemChecked(position, true);
-        drawerLayout.closeDrawer(drawerList);
-
-        switch (position){
-            case 0:
-//                startActivity(new Intent(this, Feature01Activity.class));
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_feature01:
+                startActivity(new Intent(this, MenuActivity.class));
                 break;
-            case 1:
+            case R.id.nav_feature02:
                 startActivity(new Intent(this, Feature02Activity.class));
                 break;
-            case 2:
+            case R.id.nav_feature03:
                 startActivity(new Intent(this, Feature03Activity.class));
                 break;
-            case 3:
+            case R.id.nav_feature04:
                 startActivity(new Intent(this, Feature04Activity.class));
                 break;
-            case 4:
+            case R.id.nav_feature05:
                 startActivity(new Intent(this, Feature05Activity.class));
                 break;
-            case 5:
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login_App.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.nav_profile:
                 startActivity(new Intent(this, Feature06Activity.class));
                 break;
         }
-
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
