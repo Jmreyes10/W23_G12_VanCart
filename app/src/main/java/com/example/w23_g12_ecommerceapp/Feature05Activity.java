31ad5e1dfe90cfe0cefc4b89a5e3557d8bedd800
jmrf1 <reyesfigueroaj@student.douglascollege.ca>
2023-04-05 10:00:40 -0700

    - Changed menu option title from "Home" to "View Items" - Changed menu option title from "FAQs" to "Rate Us" - Created RatingActivity to caputure the ratings of the cusomer on 4 different aspects of the app - Created and saved in Raw folder a cvs file with simulated historical ratings data - Created a DashBoardActivity with the following functionalities: 	- Read the historical ratings file 	- Append the ratings given by the current app user 	- Display a dashboard with 4 pie charts showing the rates distribution for the correspondent aspect and one bar chart showing the average rating for each of the 4 aspects 	- Included a button to go back to the main menu

--- app/src/main/java/com/example/w23_g12_ecommerceapp/Feature05Activity.java
+++ app/src/main/java/com/example/w23_g12_ecommerceapp/Feature05Activity.java
@@ -4,10 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 
 public class Feature05Activity extends AppCompatActivity {
+
+    RatingBar ratingBarQ1, ratingBarQ2, ratingBarQ3, ratingBarQ4;
+    ImageView ivRatingQ1, ivRatingQ2, ivRatingQ3, ivRatingQ4;
+    Button btnSumbitRating;
+    SharedPreferences mPreferences;
+    SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,98 @@ public class Feature05Activity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+
+
+        ratingBarQ1 = findViewById(R.id.ratingBarQ1);
+        ratingBarQ2 = findViewById(R.id.ratingBarQ2);
+        ratingBarQ3 = findViewById(R.id.ratingBarQ3);
+        ratingBarQ4 = findViewById(R.id.ratingBarQ4);
+
+        ivRatingQ1 = findViewById(R.id.ivRatingQ1);
+        ivRatingQ2 = findViewById(R.id.ivRatingQ2);
+        ivRatingQ3 = findViewById(R.id.ivRatingQ3);
+        ivRatingQ4 = findViewById(R.id.ivRatingQ4);
+
+        btnSumbitRating = findViewById(R.id.btnSumbitRating);
+
+        ratingBarQ1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if (ratingBarQ1.getRating()<=1)
+                    ivRatingQ1.setImageResource(R.drawable.img_rating_very_bad);
+                else if (ratingBarQ1.getRating()<=2)
+                    ivRatingQ1.setImageResource(R.drawable.img_rating_poor);
+                else if (ratingBarQ1.getRating()<=3)
+                    ivRatingQ1.setImageResource(R.drawable.img_rating_medium);
+                else if (ratingBarQ1.getRating()<=4)
+                    ivRatingQ1.setImageResource(R.drawable.img_rating_good);
+                else
+                    ivRatingQ1.setImageResource(R.drawable.img_rating_excellent);
+            }
+        });
+
+        ratingBarQ2.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if (ratingBarQ2.getRating()<=1)
+                    ivRatingQ2.setImageResource(R.drawable.img_rating_very_bad);
+                else if (ratingBarQ2.getRating()<=2)
+                    ivRatingQ2.setImageResource(R.drawable.img_rating_poor);
+                else if (ratingBarQ2.getRating()<=3)
+                    ivRatingQ2.setImageResource(R.drawable.img_rating_medium);
+                else if (ratingBarQ2.getRating()<=4)
+                    ivRatingQ2.setImageResource(R.drawable.img_rating_good);
+                else
+                    ivRatingQ2.setImageResource(R.drawable.img_rating_excellent);
+            }
+        });
+
+        ratingBarQ3.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if (ratingBarQ3.getRating()<=1)
+                    ivRatingQ3.setImageResource(R.drawable.img_rating_very_bad);
+                else if (ratingBarQ3.getRating()<=2)
+                    ivRatingQ3.setImageResource(R.drawable.img_rating_poor);
+                else if (ratingBarQ3.getRating()<=3)
+                    ivRatingQ3.setImageResource(R.drawable.img_rating_medium);
+                else if (ratingBarQ3.getRating()<=4)
+                    ivRatingQ3.setImageResource(R.drawable.img_rating_good);
+                else
+                    ivRatingQ3.setImageResource(R.drawable.img_rating_excellent);
+            }
+        });
+
+        ratingBarQ4.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if (ratingBarQ4.getRating()<=1)
+                    ivRatingQ4.setImageResource(R.drawable.img_rating_very_bad);
+                else if (ratingBarQ4.getRating()<=2)
+                    ivRatingQ4.setImageResource(R.drawable.img_rating_poor);
+                else if (ratingBarQ4.getRating()<=3)
+                    ivRatingQ4.setImageResource(R.drawable.img_rating_medium);
+                else if (ratingBarQ4.getRating()<=4)
+                    ivRatingQ4.setImageResource(R.drawable.img_rating_good);
+                else
+                    ivRatingQ4.setImageResource(R.drawable.img_rating_excellent);
+            }
+        });
+
+        btnSumbitRating.setOnClickListener((View v) -> {
+            mEditor.putFloat("ratingQ1", ratingBarQ1.getRating());
+            mEditor.putFloat("ratingQ2", ratingBarQ1.getRating());
+            mEditor.putFloat("ratingQ3", ratingBarQ1.getRating());
+            mEditor.putFloat("ratingQ4", ratingBarQ1.getRating());
+            mEditor.commit();
+
+            Intent intent = new Intent(this, DashboardActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 
     @Override
